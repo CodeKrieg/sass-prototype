7c834fa0adafe4ce3c08095dbee4c247928af670
startup_app <paul_badarau@yahoo.com>
2016-03-08 20:54:15 +0000

    Add gradiant to body, edit sign in, sign up devise pages, design navigation and footer

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,15 +1,17 @@
 class CallbacksController < Devise::OmniauthCallbacksController
+    
+    # Callback example
     # def digitalocean
     #     @user = User.from_omniauth(request.env["omniauth.auth"])
     #     sign_in_and_redirect @user
     # end
     
-    def facebook
+    def facebook #Callback for Facebook
        @user = User.from_omniauth(request.env["omniauth.auth"])
         sign_in_and_redirect @user 
     end
     
-    def github
+    def github #Callback for GitHub
         @user = User.from_omniauth(request.env["omniauth.auth"])
         sign_in_and_redirect @user 
     end 
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,4 @@ Rails.application.routes.draw do
   #Root route
   root 'welcome#index'
   
-  
-
 end
